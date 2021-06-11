e880a6a35f077e7282af362ae328e5935afb9bfe
yoliskdeveloper <yolisk.developer@gmail.com>
2021-06-11 17:56:00 +0700

    delete file animal in main directory

--- animal_farm.py
+++
@@ -1,8 +0,0 @@
-from animals import lion
-
-    if kind == 'dog':
-        return dog.Dog()
-    if kind == 'lion':
-        return lion.Lion()
-    if kind == 'leopard':
-        return leopard.Leopard()
